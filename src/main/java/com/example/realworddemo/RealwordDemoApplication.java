6eb6d5c1386905c62691141ad3a169f00b2b5523
Nguyen Tung Lam <tunglam20132208@gmail.com>
2020-06-03 10:28:20 +0700

    init

---
+++ src/main/java/com/example/realworddemo/RealwordDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.realworddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealwordDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealwordDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/realworddemo/RealwordDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realworddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealwordDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
